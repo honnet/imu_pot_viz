7aef78bbbe2e7c27b26ebb0a927b180a21799b3e
Cedric Honnet <honnet@telecom-paristech.org>
2020-09-13 18:09:17 -0400

    Adapt to teensy 2++ and send euler angles + potentiometer values.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,41 +3,41 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-#define BNO055_SAMPLERATE_DELAY_MS (100)
+#define BNO055_SAMPLERATE_DELAY_MS (50)
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x29);  // id, address
 
 void setup(void)
 {
+    pinMode(27, OUTPUT); digitalWrite(27, HIGH);  // Pin to power the IMU
+    pinMode(38, OUTPUT); digitalWrite(38, HIGH);  // Pin to power the potentiometer
+
+    while(!Serial); // wait
     Serial.begin(115200);
-    pinMode(A7, OUTPUT); digitalWrite(A7, HIGH);  // emulated VCC
-    pinMode(A6, OUTPUT); digitalWrite(A6, LOW);   // emulated GND
 
     if(!bno.begin()) {
         while(1) {
             Serial.println("No BNO055 detected... Check wiring or I2C ADDR!");
-            delay(BNO055_SAMPLERATE_DELAY_MS);
+            delay(1000);
         }
     }
-
-    bno.setExtCrystalUse(true);                                                             // TODO test without
+    bno.setExtCrystalUse(true); // TODO test both
 }
 
 void loop(void)
 {
-    imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-
-    // http://www.hobbytronics.co.uk/accelerometer-info
-    float tilt_x = atan2( accel.y() , sqrt(accel.x()*accel.x() + accel.z()*accel.z()) );
-    float tilt_y = atan2( accel.y() , sqrt(accel.x()*accel.x() + accel.z()*accel.z()) );
-
-    Serial.println( tilt_x * (180.0 / M_PI) );                                               // TODO: leave in rad afterward
-
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print( euler.x() ); Serial.print( '\t' ); // TODO: pick only 1
+    Serial.print( euler.y() ); Serial.print( '\t' ); // TODO: pick only 1
+    Serial.print( euler.z() ); Serial.print( '\t' ); // TODO: pick only 1
+    Serial.print( analogRead(A1) ); Serial.print( '\n' );
     delay(BNO055_SAMPLERATE_DELAY_MS);
 }
 
-
-                                                                                            // TODO: try ESP + MPU ?
-                                                                                            //       D6 ?
-
+/*
+Orientation conventions:
+   - Pitch: -180° to +180° (turing clockwise increases values)
+   - Roll: -90° to +90° (increasing with increasing inclination)
+   - Yaw: 0° to 360° (turning clockwise increases values)
+*/

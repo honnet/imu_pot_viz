6f07a73c634288a6b2130a35c86be787b0808b87
Cedric Honnet <honnet@telecom-paristech.org>
2020-12-02 20:20:07 -0500

    Add 2nd IMU.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,22 +6,24 @@
 #define BNO055_SAMPLERATE_DELAY_MS (1)
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x29);  // id, address
-
+Adafruit_BNO055 bno[] = { Adafruit_BNO055(-1, 0x28),    // addr pin at 0
+                          Adafruit_BNO055(-1, 0x29) };  // addr pin at 1
+const int bno_num = sizeof bno / sizeof bno[0];
 const int LED = 13;
 const int pots[] = {A0, A1};
 
 
 void setup(void)
 {
+    pinMode(A2, OUTPUT); digitalWrite(A2, HIGH);  // Pin to power the IMU
     pinMode(A3, OUTPUT); digitalWrite(A3, HIGH);  // Pin to power the IMU
     pinMode(LED, OUTPUT);
 
     while(!Serial); // wait
     Serial.begin(115200);
 
-    if(!bno.begin()) {
-        while(1) {
+    for (int i = 0; i < bno_num; i++) {
+        while(!bno[i].begin()) {
             Serial.println("No BNO055 detected!");
             delay(1000);
         }
@@ -30,8 +32,9 @@ void setup(void)
 
 void loop(void)
 {
-    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-    Serial.print( euler.z() ); Serial.print( '\t' );
+    int euler_diff = bno[1].getVector(Adafruit_BNO055::VECTOR_EULER).z() -
+                     bno[0].getVector(Adafruit_BNO055::VECTOR_EULER).z();
+    Serial.print( euler_diff );         Serial.print( '\t' );
     Serial.print( analogRead(pots[0])); Serial.print( '\t' );
     Serial.print( analogRead(pots[1])); Serial.print( '\n' );
 
